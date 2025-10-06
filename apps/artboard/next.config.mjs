e258a0379156935f1cd5aabaec545e2d04e0cff3
aambekar234 <ado-03bearers@icloud.com>
2025-10-05 19:09:14 -0500

    resolved build error

--- apps/artboard/next.config.mjs
+++ apps/artboard/next.config.mjs
@@ -2,23 +2,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    const clientDomain = process.env.CLIENT_DOMAIN?.replace(/\/$/, ""); // trim trailing slash if any
-
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `frame-ancestors ${clientDomain || "'self'"};`,
-          },
-          // Clear or override any restrictive defaults
-          { key: "X-Frame-Options", value: "ALLOWALL" },
-        ],
-      },
-    ];
-  },
   transpilePackages: ["@myorg/ui"],
 };
 
